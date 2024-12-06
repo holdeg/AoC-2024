aec182cdf216fa64a7b656b1159a98e23318218d
George Holderness <gholderness@microsoft.com>
2024-12-06 11:46:25 +0000

    day 2: part 1

--- src/day02.rs
+++ src/day02.rs
@@ -4,19 +4,47 @@ use crate::Solution;
 pub struct Day02;
 
 impl Solution for Day02 {
-    type ParsedInput = String;
+    type ParsedInput = Vec<Vec<i8>>;
 
     fn parse_input(input_lines: &str) -> Self::ParsedInput {
         // Change the return type of this function by editing the ParsedInput type above.
         // You can skip this and pass the raw string to each part.
         // Alternatively, you can parse the input here, either working on the same mutable struct
         // in parts one and two or passing a tuple with the data required for each part.
-        input_lines.to_string()
+
+        input_lines
+            .lines()
+            .map(|line| {
+                line.split_whitespace()
+                    .map(|num| num.parse().unwrap())
+                    .collect()
+            })
+            .collect()
     }
 
-    fn part_one(_parsed_input: &mut Self::ParsedInput) -> String {
-        // TODO: implement part one
-        0.to_string()
+    fn part_one(parsed_input: &mut Self::ParsedInput) -> String {
+        parsed_input
+            .iter()
+            .map(|report| {
+                let distances = report
+                    .iter()
+                    .zip(report.iter().skip(1))
+                    .map(|(current, next)| next - current);
+                if distances
+                    .clone()
+                    .any(|distance| -3 > distance || 3 < distance || distance == 0)
+                {
+                    return 0usize;
+                }
+                if distances.clone().any(|distance| distance < 0)
+                    && distances.clone().any(|distance| distance > 0)
+                {
+                    return 0usize;
+                }
+                1usize
+            })
+            .sum::<usize>()
+            .to_string()
     }
 
     fn part_two(_parsed_input: &mut Self::ParsedInput) -> String {
@@ -31,7 +59,17 @@ mod tests {
 
     #[test]
     fn check_day02_part1_case1() {
-        assert_eq!(Day02::solve_part_one(""), "0".to_string())
+        assert_eq!(
+            Day02::solve_part_one(
+                "7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9"
+            ),
+            "2".to_string()
+        )
     }
 
     #[test]
