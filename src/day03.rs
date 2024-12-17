2f8002ffd139c94f6102b10d10ab342ae5628b41
George Holderness <gholderness@microsoft.com>
2024-12-17 17:21:56 +0000

    day 3: part 1

--- src/day03.rs
+++ src/day03.rs
@@ -1,22 +1,31 @@
+use once_cell::sync::Lazy;
+use regex::Regex;
+
 use crate::Solution;
 
 #[derive(Clone, Debug)]
 pub struct Day03;
 
+static RE: Lazy<Regex> = Lazy::new(|| Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap());
+
 impl Solution for Day03 {
     type ParsedInput = String;
 
     fn parse_input(input_lines: &str) -> Self::ParsedInput {
-        // Change the return type of this function by editing the ParsedInput type above.
-        // You can skip this and pass the raw string to each part.
-        // Alternatively, you can parse the input here, either working on the same mutable struct
-        // in parts one and two or passing a tuple with the data required for each part.
-        input_lines.to_string()
+        let mut input = input_lines.to_string();
+        input.retain(|char| !char.is_whitespace());
+        input
     }
 
-    fn part_one(_parsed_input: &mut Self::ParsedInput) -> String {
-        // TODO: implement part one
-        0.to_string()
+    fn part_one(parsed_input: &mut Self::ParsedInput) -> String {
+        RE.captures_iter(parsed_input)
+            .map(|captures| {
+                captures.iter().skip(1).fold(1u32, |prod, el| {
+                    prod * el.unwrap().as_str().parse::<u32>().unwrap()
+                })
+            })
+            .sum::<u32>()
+            .to_string()
     }
 
     fn part_two(_parsed_input: &mut Self::ParsedInput) -> String {
@@ -31,7 +40,12 @@ mod tests {
 
     #[test]
     fn check_day03_part1_case1() {
-        assert_eq!(Day03::solve_part_one(""), "0".to_string())
+        assert_eq!(
+            Day03::solve_part_one(
+                "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+            ),
+            "161".to_string()
+        )
     }
 
     #[test]
