8d0c4d6142f77840918aaf8366d36660f38ff9ac
George Holderness <george.holderness1@gmail.com>
2024-12-26 00:18:28 +0000

    day 7: part 2

--- src/day07.rs
+++ src/day07.rs
@@ -4,6 +4,7 @@ use crate::Solution;
 pub enum Operator {
     Plus,
     Multiply,
+    Concatenate,
 }
 
 impl Operator {
@@ -11,6 +12,7 @@ impl Operator {
         match self {
             Self::Plus => op1 + op2,
             Self::Multiply => op1 * op2,
+            Self::Concatenate => (op1.to_string() + &op2.to_string()).parse().unwrap(),
         }
     }
 }
@@ -71,9 +73,37 @@ impl Solution for Day07 {
             .to_string()
     }
 
-    fn part_two(_parsed_input: &mut Self::ParsedInput) -> String {
-        // TODO: implement part two
-        0.to_string()
+    fn part_two(parsed_input: &mut Self::ParsedInput) -> String {
+        parsed_input
+            .iter()
+            .filter(|(goal, operands)| {
+                let num_operators = operands.len() - 1;
+                let mut operator_permutations = vec![vec![]];
+                for _ in 0..num_operators {
+                    let mut next_wave = vec![];
+                    while let Some(proto) = operator_permutations.pop() {
+                        for operator in [Operator::Multiply, Operator::Plus, Operator::Concatenate]
+                        {
+                            let mut new_vector = proto.clone();
+                            new_vector.push(operator);
+                            next_wave.push(new_vector);
+                        }
+                    }
+                    operator_permutations = next_wave;
+                }
+
+                operator_permutations.iter().any(|permutation| {
+                    let operands_iter = operands.iter().map(|el| *el);
+                    let mut operator_iter = permutation.into_iter();
+                    operands_iter
+                        .reduce(|acc, operand| operator_iter.next().unwrap().apply(acc, operand))
+                        .unwrap()
+                        == *goal
+                })
+            })
+            .map(|(goal, _)| goal)
+            .sum::<i64>()
+            .to_string()
     }
 }
 
@@ -101,7 +131,20 @@ mod tests {
 
     #[test]
     fn check_day07_part2_case1() {
-        assert_eq!(Day07::solve_part_two(""), "0".to_string())
+        assert_eq!(
+            Day07::solve_part_two(
+                "190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20"
+            ),
+            "11387".to_string()
+        )
     }
 
     #[test]
