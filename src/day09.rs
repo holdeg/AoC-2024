41a65d4f19bb10815291f4eab8ebf5d5ada0c14f
George Holderness <george.holderness1@gmail.com>
2024-12-26 14:55:48 +0000

    day 9: part 1

--- src/day09.rs
+++ src/day09.rs
@@ -1,22 +1,50 @@
+use itertools::Itertools;
+
 use crate::Solution;
 
 #[derive(Clone, Debug)]
 pub struct Day09;
 
 impl Solution for Day09 {
-    type ParsedInput = String;
+    type ParsedInput = Vec<i64>;
 
     fn parse_input(input_lines: &str) -> Self::ParsedInput {
-        // Change the return type of this function by editing the ParsedInput type above.
-        // You can skip this and pass the raw string to each part.
-        // Alternatively, you can parse the input here, either working on the same mutable struct
-        // in parts one and two or passing a tuple with the data required for each part.
-        input_lines.to_string()
+        let mut file = true;
+        let mut id = 0;
+        input_lines
+            .chars()
+            .flat_map(|ch| {
+                let digit = ch.to_digit(10).unwrap();
+                if file {
+                    file = false;
+                    vec![id; digit as usize]
+                } else {
+                    file = true;
+                    id += 1;
+                    vec![-1; digit as usize]
+                }
+            })
+            .collect_vec()
     }
 
-    fn part_one(_parsed_input: &mut Self::ParsedInput) -> String {
-        // TODO: implement part one
-        0.to_string()
+    fn part_one(parsed_input: &mut Self::ParsedInput) -> String {
+        let file_blocks = parsed_input.iter().filter(|id| **id != -1).count();
+        let is_compacted = |disk: Vec<i64>| {
+            disk.iter()
+                .position(|id| *id == -1)
+                .is_some_and(|idx| idx == file_blocks)
+        };
+        while !is_compacted(parsed_input.clone()) {
+            let first_empty_block = parsed_input.iter().position(|id| *id == -1).unwrap();
+            let last_file_block = parsed_input.iter().rposition(|id| *id != -1).unwrap();
+            parsed_input.swap(first_empty_block, last_file_block);
+        }
+        parsed_input
+            .into_iter()
+            .filter(|id| **id != -1)
+            .enumerate()
+            .fold(0, |acc, (idx, id)| acc + idx as i64 * *id)
+            .to_string()
     }
 
     fn part_two(_parsed_input: &mut Self::ParsedInput) -> String {
@@ -31,7 +59,10 @@ mod tests {
 
     #[test]
     fn check_day09_part1_case1() {
-        assert_eq!(Day09::solve_part_one(""), "0".to_string())
+        assert_eq!(
+            Day09::solve_part_one("2333133121414131402"),
+            "1928".to_string()
+        )
     }
 
     #[test]
